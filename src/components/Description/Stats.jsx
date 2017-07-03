4e7caf96b405cb551bba732f853575a31acc32fd
Nicholas Mak <nicholas123mak@gmail.com>
2017-07-03 15:52:47 -0400

    fixed zindex issue

--- src/components/Description/Stats.jsx
+++ src/components/Description/Stats.jsx
@@ -16,16 +16,16 @@ class Stats extends Component {
         const boxOffice = <span>{formatter.format(movieObject.revenue)|| "--" }</span>
 		return (
           <Row className='StatsContainer'>
-            <Col xs={12} xsPush={3} sm={6} smPush={1} lg={6}>
+            <Col xs={6} sm={6} smPush={1} lg={6}>
              <h4>Vote Average:</h4><br/><span>{ movieObject.vote_average || "--" }</span>
             </Col>
-            <Col xs={12} xsPush={3} sm={6} smPush={1} lg={6}>
+            <Col xs={6}  sm={6} smPush={1} lg={6}>
               <h4>Original Release</h4><br/> <span>{ movieObject.release_date || "--" } </span>
             </Col>
-            <Col xs={12}xsPush={3} sm={6} smPush={1} lg={6}>
+            <Col xs={6} sm={6} smPush={1} lg={6}>
               <h4>Running Time:</h4><br/> <span>{ movieObject.runtime || "--" } minutes</span>
             </Col>
-            <Col xs={12}xsPush={3} sm={6} smPush={1} lg={6}>
+            <Col xs={6} sm={6} smPush={1} lg={6}>
              <h4>Box Office</h4><br/>{boxOffice}
             </Col>
           </Row>
